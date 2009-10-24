78918a3d79a8d62adb6722ef49d9a52a13303009
Ryan Voots <simcop2387@yahoo.com>
2009-10-23 20:20:31 -0400

    samplecode collection code is done, moving it to Detect

--- test.pl
+++ test.pl
@@ -25,10 +25,44 @@ my @spects = FFTW::getfftw($audio, "tmp");
 my $i = 0;
 
 my @voicemap = map {Detect::hasvoice($_, $i++)} @spects;
-
-my $map1 = Detect::cleanup(join "", @voicemap);
+my $map0 = join "", @voicemap;
+my $map1 = Detect::cleanup($map0);
 my $map2 = Detect::cleanup($map1);
 
-print $map1;
-print "-------------------\n";
-print $map2;
+print $map2,"\n";
+
+my @codes;
+
+#i'm gonna use map2 here, to get a good list
+$i=-1; #start at -1 for this
+my $start=0;
+my $finish=0;
+my $collecting=1;
+for my $sample (split//,$map2)
+{
+ $i++;
+ next if ($sample == 1 && $collecting == 1); #don't do anything
+ 
+ if ($sample == 0 && $collecting == 1)
+ {
+  $finish = $i; #gets it +1
+  $collecting = 0;
+  push @codes, [$start, $finish];
+  next;
+ }
+ 
+ if ($sample == 1 && $collecting == 0)
+ {
+   $start = $i-1; #get 1 before this sample
+   $collecting = 1;
+   next;
+ }
+}
+
+if ($collecting == 1)
+{
+  push @codes, [$start, $i];
+}; #collect the last end of it
+
+
+print Dumper(\@codes);
